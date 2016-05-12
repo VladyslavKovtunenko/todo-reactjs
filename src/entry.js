5932ed8387f402c476515f013db875d8ad61747b
VladyslavKovtunenko <kovtunenkovladyslav@gmail.com>
2016-05-12 17:59:12 +0300

    list view

--- src/entry.js
+++ src/entry.js
@@ -1,12 +1,22 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-console.log(document.getElementById('container'));
 
+const tasks = [{title: 'First task', priority: 'normal'}, {title: 'Second task', priority: 'critical'}];
 var container = document.getElementById('container');
 var App = React.createClass({
     render: function () {
-        return <h1>Hello, world!</h1>;
+        console.log(this.props.list);
+        console.log(this.props.list.length);
+
+        var string = [];
+
+        for (let i = 0; i < this.props.list.length; i++) {
+            string[i] = React.createElement('li', null, this.props.list[i].title);
+        }
+        var tmp = React.createElement('ol', null, string);
+        // console.log(React.DOM.p(null, tmp));
+        return React.DOM.p(null, tmp);
     }
 });
 
-ReactDOM.render(<App />, document.getElementById('container'));
+ReactDOM.render(<App list={tasks}/>, document.getElementById('container'));
