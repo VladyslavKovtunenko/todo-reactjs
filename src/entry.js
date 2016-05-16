c72d3c7e6eeeb33b34028b788c89f15d076b7870
VladyslavKovtunenko <kovtunenkovladyslav@gmail.com>
2016-05-16 17:25:16 +0300

    delete React.DOM.p

--- src/entry.js
+++ src/entry.js
@@ -2,14 +2,13 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 
 const tasks = [{title: 'First task', priority: 'normal'}, {title: 'Second task', priority: 'critical'}];
-var container = document.getElementById('container');
 var App = React.createClass({
     render: function () {
         var string = this.props.list.map((task, i) => (
             <li style={{backgroundColor: task.priority === 'normal' ? 'green' : 'red'}}>{this.props.list[i].title}</li>
         ));
 
-        return React.DOM.p(null, <ol>{string}</ol>);
+        return <ol>{string}</ol>;
     }
 });
 
