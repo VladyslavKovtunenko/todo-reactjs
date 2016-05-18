2f1db50c499df11263729c239e802e7e237e1dfc
VladyslavKovtunenko <kovtunenkovladyslav@gmail.com>
2016-05-18 16:07:31 +0300

    refactor

--- src/listComponent.js
+++ src/listComponent.js
@@ -4,49 +4,54 @@ import {Task} from './taskComponent'
 import {tasks} from './tasksData'
 
 export let List = React.createClass({
+    getInitialState: function () {
+        return {};
+    },
 
+    addNewTask: function (e) {
+        e.preventDefault();
 
-    addNewTask: function () {
-        /*tasks.push({
-         title: document.getElementById('title').value,
-         priority: document.getElementById('priority').value
-         });*/
-        console.log(tasks);
-        /**
-         * TODO delete next line
-         */
-        //ReactDOM.render(<List list={tasks}/>, document.getElementById('container'));
-        //document.getElementById('title').value = '';
+        if (this.taskTitle.value !== '') {
+            this.setState({
+                title: this.taskTitle.value,
+                priority: this.taskPriority.value
+            });
+            /**
+             * TODO delete next line
+             */
+            // ReactDOM.render(<List list={tasks}/>, document.getElementById('container'));
+            this.taskTitle.value = '';
+        }
     },
 
     render: function () {
+        this.state.tasks = this.props.list;
         return (
             <div>
-                <form onsubmit={this.addNewTask()}>
+                <form onSubmit={this.addNewTask.bind(this)}>
                     <dl>
                         <dd>
                             <h4>Add task</h4>
                         </dd>
                         <dd>
                             <h5>Task title</h5>
-                            <input type='text' id='title' placeholder='Title'/>
+                            <input type='text' placeholder='Title' ref={(ref) => this.taskTitle = ref}/>
                         </dd>
                         <dd>
                             <h5>Priority</h5>
-                            <select id='priority'>
+                            <select ref={(ref) => this.taskPriority = ref}>
                                 <option>normal</option>
                                 <option>critical</option>
                             </select>
                         </dd>
                         <dd>
-                            <input type='submit' id='submit'/>
+                            <input type='submit' value="Submit"/>
                         </dd>
                     </dl>
                 </form>
-                <ol id='list'>
-                    {this.props.list.map((task, index) => (<Task task={task} index={index}/>))}
+                <ol>
+                    {this.state.tasks.map((task, index) => (<Task task={task} index={index}/>))}
                 </ol>
             </div>);
     }
-
 });
--- src/taskComponent.js
+++ src/taskComponent.js
@@ -5,7 +5,7 @@ export let Task = React.createClass({
         return (
             <li style={{backgroundColor: this.props.task.priority=== 'normal' ? 'green' : 'red'}}>
                 {this.props.task.title}
-            <button type='button' data-action='delete' id={this.props.index}>Delete</button>
+                <button type='button' data-action='delete' id={this.props.index}>Delete</button>
             </li>);
     }
 });
