8c290f257890741380c98f930741fd5baede8909
VladyslavKovtunenko <kovtunenkovladyslav@gmail.com>
2016-05-24 16:04:25 +0300

    refactor

--- components/app.js
+++ components/app.js
@@ -1,12 +1,50 @@
 import React from 'react'
-import {TaskList} from '../containers/taskList'
-import {InputTask} from '../containers/inputTask'
+import {addTask} from '../actions/actions'
+import {Task} from './task'
 
 export let App = (store) => {
+    let input;
+    let state = store.store;
+    let tasks = state.getState();
+
     return (
         <div>
-            <InputTask store={store}/>
-            <TaskList tasks={store.store.getState()}/>
+            <form onSubmit={
+                e => {
+                    e.preventDefault();
+                    if (!input.value.trim()) {
+                        return;
+                    }
+                    state.dispatch(addTask(input.value));
+                    input.value = '';
+                }
+            }>
+                <dl>
+                    <dd>
+                        <h4>Add task</h4>
+                    </dd>
+                    <dd>
+                        <h5>Task title</h5>
+                        <input ref={node => {input = node}}/>
+                    </dd>
+                    <dd>
+                        <select>
+                            <option>normal</option>
+                            <option>critical</option>
+                        </select>
+                    </dd>
+                    <dd>
+                        <input type='submit' value="Submit"/>
+                    </dd>
+                </dl>
+            </form>
+            <ol>
+                {tasks.map((task) => (
+                    <li>
+                        <Task task={task}/>
+                    </li>
+                ))}
+            </ol>
         </div>
     )
 };
